08d541f7222a29643ebc50625337116b5e18262c
Daniel Canuto <danielpscanuto83@gmail.com>
2024-05-24 16:21:02 -0300

    base dois

--- facetas/admin.py
+++ facetas/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import NivelOperacional, CargoGestores, Orgao, GestaoPrincial, UnidadeAdministrativa
+from .models import NivelOperacional, CargoGestores, Orgao, GestaoPrincial, UnidadeAdministrativa, GestorEstruturaPrincipal, Paises, Estado, Municipio
 # Register your models here.
 
 admin.site.register(NivelOperacional)
+admin.site.register(Paises)
+admin.site.register(Estado)
+admin.site.register(Municipio)
+admin.site.register(GestorEstruturaPrincipal)
 admin.site.register(CargoGestores)
 admin.site.register(Orgao)
 admin.site.register(GestaoPrincial)
--- facetas/models.py
+++ facetas/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
 class NivelOperacional(models.Model):
@@ -17,15 +18,54 @@ class CargoGestores(models.Model):
     
     def __str__(self):
         return self.nome_cargo
+    
+class Paises (models.Model):
+    nome_do_pais = models.CharField(max_length=50, default='Brasil')
+    sigla = models.CharField(max_length=2, default="BR")
+    observacao = models.TextField() 
+
+    def __str__(self):
+        return self.nome_do_pais
+
+class Estado (models.Model):
+    nacionalidade = models.ForeignKey(Paises, on_delete=models.CASCADE, blank=True, null=True)
+    nome_estado = models.CharField(max_length=50)
+    sigla_estado = models.CharField(max_length=2)
+    observacao = models.TextField()
+
+    def __str__(self):
+        return self.nome_estado
+
+class Municipio(models.Model):
+    estado = models.ForeignKey(Estado, on_delete=models.CASCADE, blank=True, null=True)
+    nome_municipio = models.CharField(max_length=50)
+    observacao = models.TextField()
+    
+    def __str__(self):
+        return f'{self.nome_municipio} - {self.estado} -{self.estado.nacionalidade}'
 
 
 
-class Orgao(models.Model):
-    nome_orgao = models.CharField(max_length=100)
+# Essa classe devera cadastrar o gestor do Período 
+class GestorEstruturaPrincipal(models.Model):
+    nome = models.CharField(max_length=50)
+    nascimento = models.DateField(default=date.today)
+    naturalidade = models.ForeignKey(Municipio, on_delete=models.CASCADE, blank=True, null=True)
+    observacao = models.TextField()
+
+    def __str__(self):
+        return self.nome
+
+#A Classe representa o momento historico do organização 
+# história do todo, o elo perdido
+
+class Orgao(models.Model): 
+    nome_orgao = models.CharField(max_length=100) # incluir nome do campo denominação do período histórico
     sigla_orgao = models.CharField(max_length=10)
-    orgao_superior = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
+    orgao_superior = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True) 
     inicio_atividades = models.DateField()
     termino_atividades = models.DateField()
+    gestor = models.ForeignKey(GestorEstruturaPrincipal, on_delete=models.CASCADE, blank=True, null=True)
     cargo_gestor = models.ForeignKey(CargoGestores, on_delete=models.CASCADE, blank=True, null=True)
     fundamentacao_legal = models.TextField()
     upload_fundamentacao = models.FileField(upload_to='fundamentacao/')
@@ -34,7 +74,7 @@ class Orgao(models.Model):
     def __str__(self):
         return self.nome_orgao
 
-
+# A classe representa o momento historico da organização
 class GestaoPrincial(models.Model):
     nome_orgao_principal = models.CharField(max_length=100)
     sigla_orgao = models.CharField(max_length=10)
